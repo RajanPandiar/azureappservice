efd46cfc144a8d44c107efdb6c89963b41b5ac21
rajan <rajarajanpandiar2012@outlook.com>
2023-03-29 13:38:33 +0530

    changes

---
+++ src/main/java/com/raj/azureapp/todo/TodoApplication.java
@@ -0,0 +1,28 @@
+package com.raj.azureapp.todo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class TodoApplication{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TodoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/com/raj/azureapp/todo/UserController.java
@@ -0,0 +1,60 @@
+package com.raj.azureapp.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController
+{
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try{
+            User newUser = new User(user.getId(),user.getFirstName(),user.getLastName(), user.getAddress());
+            userRepository.save(newUser);
+            return new ResponseEntity<>(newUser,HttpStatus.CREATED);
+        }catch (Exception e) {
+            throw  new RuntimeException();
+        }
+    }
+
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers() {
+        try{
+            List<User> usersList = new ArrayList<User>();
+            userRepository.findAll().forEach(usersList::add);
+            return new ResponseEntity<>(usersList, HttpStatus.OK);
+        }catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") String id) {
+        try{
+
+            Optional<User> user  = userRepository.findById(id);
+            if (user.isPresent()) {
+                return new ResponseEntity<>(user.get(),HttpStatus.OK);
+            }
+            else  {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+}
