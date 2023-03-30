8b595a80a505a6bf02c8cab334f78bc70a577af0
rajan <rajarajanpandiar2012@outlook.com>
2023-03-30 14:37:49 +0530

    new change

--- src/main/java/com/raj/azureapp/todo/UserController.java
+++ src/main/java/com/raj/azureapp/todo/UserController.java
@@ -7,46 +7,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UserController
 {
-
     @Autowired
     UserRepository userRepository;
 
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        try{
-            System.out.println("Hi There");
-            User newUser = new User(user.getId(),user.getFirstName(),user.getLastName(), user.getAddress());
-            userRepository.save(newUser);
-            return new ResponseEntity<>(newUser,HttpStatus.CREATED);
-        }catch (Exception e) {
-            throw  new RuntimeException();
-        }
-    }
-
-    @GetMapping({"/api/v1/users"})
+    @GetMapping({"/"})
     public ResponseEntity<List<User>> getAllUsers() {
         try{
-            List<User> usersList = new ArrayList<User>();
+            List<User> usersList = new ArrayList<>();
             userRepository.findAll().forEach(usersList::add);
             return new ResponseEntity<>(usersList, HttpStatus.OK);
-        }catch (Exception e) {
-            throw new RuntimeException();
-        }
-    }
-
-
-    @GetMapping({"/api/v1/users/{id}"})
-    public ResponseEntity<User> getUser(@PathVariable String id) {
-        try{
-            Optional<User> user= userRepository.findById(id);
-            return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
-
         }catch (Exception e) {
             throw new RuntimeException();
         }
